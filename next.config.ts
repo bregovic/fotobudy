0fd14001673ec9658565536fd6ca35622e053022
bregovic <vac.kral@gmail.com>
2026-02-20 21:05:47 +0100

    chore(config): ignore local-service and log output paths from webpack dev server watch lists to prevent endless Next.js compiling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: [
+          '**/node_modules/**',
+          '**/.git/**',
+          '**/.next/**',
+          '**/public/photos/**',
+          '**/debug_watcher.txt',
+          '**/sync_map.json',
+          '**/local-service/current_event.json'
+        ],
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
